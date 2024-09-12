ad8fb34553e0135c18bc4e5d20f6c9fb7ffd26de
Eric Canton <151142068+ericcd00@users.noreply.github.com>
2024-09-12 08:28:45 +0200

    Add files via upload

---
+++ scRNAseq_10x_BoneMarrow/7_scRNAseq_BM_Analysis.R
@@ -0,0 +1,193 @@
+#------------------------------------------------------------------------------# 
+##        PAPER : G. Palma et al 2024. "..."
+##  Description : This script is for performing a scRNAseq analysis.
+##       Author : María Maqueda (mmaqueda@researchmar.net) at BigSpinLab
+##                Eric Canton (ecanton@carrerasresearch.org) at BigSpinLab
+#------------------------------------------------------------------------------# 
+
+##### Import libraries #####
+
+# Included any additional libraries required i.e. openxlsx
+libraries <- c("knitr", "ggplot2", "data.table", "dplyr", "stringr", "ggpubr", 
+               "Seurat", "glmGamPoi", "clustree", "multtest","metap", 
+               "ComplexHeatmap", "RColorBrewer","ggpubr", "openxlsx", 
+               "readxl", "ggvenn", "Rmagic", "monocle3", "SeuratWrappers", 
+               "patchwork", "gprofiler2") 
+check.libraries <- is.element(libraries, installed.packages()[, 1])==FALSE
+libraries.to.install <- libraries[check.libraries]
+if (length(libraries.to.install!=0)) {
+  require(libraries.to.install)
+}
+
+success <- sapply(libraries,require, quietly = FALSE,  character.only = TRUE)
+if(length(success) != length(libraries)) {stop("A package failed to return a success in require() function.")}
+
+
+##### Import Seurat Object #####
+seu <- readRDS(file = "/home/user/Documents/Files/Projects/BM_scRNAseq/RDS/scRNAseq_CRISPR_Screening_BM_Annotated_Only2Samples.RDS")
+
+
+##### Normalization, dimensionality reduction (PCA, UMAP) and clustering over WT sample #####
+
+#Prior to normalization, dataset is split into a list of three objects, one per condition:
+
+# split the dataset into a list of two seurat objects (stim and CTRL)
+BM.list <- SplitObject(seu, split.by = "Sample_ID")
+
+CD45_4_5 <- BM.list[["CD45_4_5"]]
+CD45_BFP_4_5 <- BM.list[["CD45_BFP_4_5"]]
+
+
+# Normalization aims to remove technical factors such as the library size that 
+# may confound the real biological heterogeneity. In this case, during 
+# normalization, mitochondrial content is also removed as a  possible source 
+# of variation in our samples. SCTransform v2 regularization is used.
+
+# **This is firstly done over the WT sample**
+
+# Normalize
+CD45_4_5 <- SCTransform(CD45_4_5,
+                        vars.to.regress = "percent.mito",
+                        vst.flavor = "v2", # Default value in v5
+                        verbose = FALSE)
+
+CD45_4_5 <- RunPCA(CD45_4_5, verbose = FALSE)
+
+pct <- CD45_4_5[["pca"]]@stdev / sum(CD45_4_5[["pca"]]@stdev) * 100
+
+# Calculate cumulative percents for each PC
+cumu <- cumsum(pct)
+
+n_dim =35 # Practically gets 85% (84.97%)
+# This number of dimensions will be used for all the samples
+
+
+##### Prepare Integration #####
+
+CD45_4_5 <- SCTransform(CD45_4_5, vars.to.regress = "percent.mito", vst.flavor = "v2", verbose = FALSE) %>%
+  RunPCA(npcs =n_dim, verbose = FALSE)
+
+CD45_BFP_4_5 <- SCTransform(CD45_BFP_4_5, vars.to.regress = "percent.mito", vst.flavor = "v2", verbose = FALSE) %>%
+  RunPCA(npcs =n_dim, verbose = FALSE)
+
+BM.list <- list(CD45_4_5 = CD45_4_5, CD45_BFP_4_5 = CD45_BFP_4_5)
+
+features <- SelectIntegrationFeatures(object.list = BM.list, nfeatures = 3000)
+BM.list <- PrepSCTIntegration(object.list = BM.list, anchor.features = features)
+
+BM.anchors <- FindIntegrationAnchors(object.list = BM.list, normalization.method = "SCT",
+                                     anchor.features = features)
+
+
+##### Integrate Samples #####
+
+BM.combined.sct <- IntegrateData(anchorset = BM.anchors, normalization.method = "SCT")
+
+BM.combined.sct <- RunPCA(BM.combined.sct, verbose = FALSE)
+
+pct <- BM.combined.sct[["pca"]]@stdev / sum(BM.combined.sct[["pca"]]@stdev) * 100
+
+# # Calculate cumulative percents for each PC
+cumu <- cumsum(pct)
+
+n_dim = 34 # 85.25% captured variance
+BM.combined.sct <- RunUMAP(BM.combined.sct, reduction = "pca",
+                           dims = 1:n_dim, verbose = FALSE)
+
+BM.combined.sct <- FindNeighbors(BM.combined.sct, reduction = "pca",
+                                 dims = 1:n_dim)
+
+BM.combined.sct <- FindClusters(BM.combined.sct, resolution = seq(0.1, 1, by=0.1))
+
+BM.combined.sct <- Seurat::SetIdent(BM.combined.sct, value = BM.combined.sct$integrated_snn_res.0.4)
+
+
+##### Subclustering #####
+
+BM.combined.sct <- Seurat::SetIdent(BM.combined.sct, value = BM.combined.sct$integrated_snn_res.0.4)
+
+BM.combined.sct <- FindSubCluster(BM.combined.sct, 11, "integrated_snn", subcluster.name = "integrated_subclust11_snn_res.0.4",  resolution = 1,   algorithm = 1)
+BM.combined.sct <- FindSubCluster(BM.combined.sct, 12, "integrated_snn", subcluster.name = "integrated_subclust12_snn_res.0.4",  resolution = 0.5, algorithm = 1)
+BM.combined.sct <- FindSubCluster(BM.combined.sct, 13, "integrated_snn", subcluster.name = "integrated_subclust13_snn_res.0.4",  resolution = 1,   algorithm = 1)
+BM.combined.sct <- FindSubCluster(BM.combined.sct, 15, "integrated_snn", subcluster.name = "integrated_subclust15_snn_res.0.4",  resolution = 0.1, algorithm = 1)
+
+
+##### Data Imputation (MAGIC) #####
+
+BM.combined.sct <- magic(BM.combined.sct, assay = "SCT")
+
+
+##### Renaming Clusters #####
+
+BM.combined.sct$curated.cell.ident <- BM.combined.sct$cell.ident
+BM.combined.sct$curated.cell.ident[BM.combined.sct$integrated_snn_res.0.4 == 9] <- "Neutrophil progenitor"
+BM.combined.sct$curated.cell.ident[BM.combined.sct$integrated_subclust11_snn_res.0.4 == "11_6"] <- "Basophil progenitor"
+BM.combined.sct$curated.cell.ident[BM.combined.sct$integrated_snn_res.0.4 == 14] <- "Innate lymphoid cell"
+
+
+##### General.curated.cell.ident #####
+
+BM.combined.sct$general.curated.cell.ident <- BM.combined.sct$curated.cell.ident
+
+BM.combined.sct@meta.data <- BM.combined.sct@meta.data %>%
+  mutate(general.curated.cell.ident = case_when(curated.cell.ident == "Myeloid cell" | curated.cell.ident == "Plasmacytoid dendritic cell" ~ "Myeloid cell",
+                                                curated.cell.ident == "Neutrophil_Mmp8 high" | curated.cell.ident == "Neutrophil_Mpo high" | curated.cell.ident == "Neutrophil_Ltf high" | 
+                                                  curated.cell.ident == "Neutrophil_Fcnb high" | curated.cell.ident == "Neutrophil_Chil3 high" | curated.cell.ident == "Neutrophil progenitor" ~ "Neutrophil",
+                                                curated.cell.ident ==  "Macrophage_Ms4a6c high" | curated.cell.ident ==  "Macrophage_Fcna high" | curated.cell.ident == "Macrophage_Ctss high" ~ "Macrophage",
+                                                curated.cell.ident ==  "Basophil" | curated.cell.ident ==  "Basophil progenitor" ~ "Basophil",
+                                                curated.cell.ident ==  "Erythroid cell" ~ "Erythroid cell",
+                                                curated.cell.ident ==  "Innate lymphoid cell" ~ "ILC",
+                                                curated.cell.ident ==  "T cell_Ccl5 high" ~ "T cell",
+                                                curated.cell.ident ==  "B cell" | curated.cell.ident ==  "Pre B cell"  ~ "B cell",
+                                                curated.cell.ident ==  "Hematopoietic stem and progenitor cell" | curated.cell.ident ==  "Hematopoietic stem cell" ~ "HSPC"))
+
+
+##### Combined subclustering annotation #####
+
+# We create a dummy variable to store metadata from the Seurat object.
+meta <- BM.combined.sct@meta.data
+
+# Each subcluster is found in a different column, so we create a new one to store all subclusters
+meta$subcluster.integrated_snn_res.0.4 <- as.character(meta$integrated_snn_res.0.4)
+
+meta$subcluster.integrated_snn_res.0.4 <- ifelse((meta$integrated_snn_res.0.4 == "11"), as.character(meta$integrated_subclust11_snn_res.0.4), as.character(meta$subcluster.integrated_snn_res.0.4))
+
+meta$subcluster.integrated_snn_res.0.4 <- ifelse((meta$integrated_snn_res.0.4 == "12"), as.character(meta$integrated_subclust12_snn_res.0.4), as.character(meta$subcluster.integrated_snn_res.0.4))
+
+meta$subcluster.integrated_snn_res.0.4 <- ifelse((meta$integrated_snn_res.0.4 == "13"), as.character(meta$integrated_subclust13_snn_res.0.4), as.character(meta$subcluster.integrated_snn_res.0.4))
+
+meta$subcluster.integrated_snn_res.0.4 <- ifelse((meta$integrated_snn_res.0.4 == "15"), as.character(meta$integrated_subclust15_snn_res.0.4), as.character(meta$subcluster.integrated_snn_res.0.4))
+
+meta <- meta[,c(1:9, 15, 10)]
+
+BM.combined.sct@meta.data <- meta
+
+
+##### Final Clusters #####
+
+# We create a dummy variable to store metadata from the Seurat object.
+meta <- BM.combined.sct@meta.data
+
+# Manual rearrangement of subclusters
+meta$final.clusters.integrated_snn <- as.character(meta$integrated_snn_res.0.4)
+
+meta$final.clusters.integrated_snn <- ifelse((meta$subcluster.integrated_snn_res.0.4 == "13_0" | meta$subcluster.integrated_snn_res.0.4 == "13_1" | meta$subcluster.integrated_snn_res.0.4 == "13_3"), "1", as.character(meta$final.clusters.integrated_snn))
+
+meta$final.clusters.integrated_snn <- ifelse((meta$subcluster.integrated_snn_res.0.4 == "11_1" | meta$subcluster.integrated_snn_res.0.4 == "11_5"), "17", as.character(meta$final.clusters.integrated_snn))
+
+meta$final.clusters.integrated_snn <- ifelse((meta$subcluster.integrated_snn_res.0.4 == "11_3"), "18", as.character(meta$final.clusters.integrated_snn))
+
+meta$final.clusters.integrated_snn <- ifelse((meta$subcluster.integrated_snn_res.0.4 == "11_6"), "19", as.character(meta$final.clusters.integrated_snn))
+
+meta$final.clusters.integrated_snn <- ifelse((meta$subcluster.integrated_snn_res.0.4 == "13_2" | meta$subcluster.integrated_snn_res.0.4 == "12_0"), "13", meta$final.clusters.integrated_snn)
+
+meta$final.clusters.integrated_snn <- ifelse((meta$subcluster.integrated_snn_res.0.4 == "11_2"), "20", as.character(meta$final.clusters.integrated_snn))
+
+meta$final.clusters.integrated_snn <- ifelse((meta$subcluster.integrated_snn_res.0.4 == "15_2" | meta$subcluster.integrated_snn_res.0.4 == "15_1"), "21", meta$final.clusters.integrated_snn)
+
+meta <- meta[,c(4,2,3,6,7,12,13,27,19,32)]
+
+BM.combined.sct@meta.data <- meta
+
+## Save RDS object
+saveRDS(BM.combined.sct,"/home/user/Documents/Files/Projects/BM_scRNAseq/RDS/scRNAseq_CRISPR_Screening_BM_Annotated_Integrated_FINAL.RDS")
---
+++ scRNAseq_10x_BoneMarrow/Preprocessing/0_CellRanger_Expression_Quantification.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+#SBATCH --job-name=CellRanger_BM_cluster
+#SBATCH --partition=fast
+#SBATCH --cpus-per-task=6
+#SBATCH --nodes=1 
+#SBATCH --mem MaxMemPerNode
+#SBATCH -t 40:00:00 
+#SBATCH -o logs/CellRanger_BM_cluster.out
+#SBATCH -e logs/CellRanger_BM_cluster.err
+
+# REMARK 1: This script is not considering Feature Barcode analysis. Code should be
+# adapted if this is required
+
+# REMARK 2: This script is considering that one same library has been sequenced in
+# different flow cells
+
+# REMARK 3: No array execution can be done since there is only one 'fast' node
+
+#=========================
+# User defined parameters: relevant paths
+#=========================
+# SPECIFY Root directory in the cluster (usually /projects/cancer)
+ROOTDIR="/projects/cancer"
+
+# SPECIFY your project working directory
+WKD=$ROOTDIR'/CRISPR_screening_mESCs_ABigas/scRNAseq_Sorted_BM'
+
+#=========================
+# General configuration
+#=========================
+START=$(date +%s)
+# Enable Singularity image to look into the general path (equivalent to -B)
+export SINGULARITY_BIND=$ROOTDIR 
+# Path to images folder in cluster
+IMAGES_PATH=$ROOTDIR"/images"
+# Path to databases folder in cluster
+DB_PATH=$ROOTDIR"/db_files"
+
+################################################################################
+##       Obtain raw gene expression information from scRNAseq 10X Genomics data
+##                          using Cell Ranger software 
+################################################################################
+
+# Check the several tutorials for CellRanger in 10X Genomics website
+# https://www.10xgenomics.com/support/software/cell-ranger/latest
+
+###########################
+## 1. Other relevant paths for I/O
+###########################
+
+# Specify path to raw data
+RAWDATA=$WKD'/raw_data'
+
+# Specify the samplesheet with info about sample name, flow cells and fastq dir
+SAMPLESHEET=$RAWDATA'/Samplesheet_x_CellRanger.txt'
+
+#################################################
+## 2. Singularity image and Tool Parametrization
+#################################################
+
+# Specify singularity image to be used
+CELLRANGER="cellranger_v7.2.0.sif"
+
+# Specify the path to the transcriptome to be used
+REF=$DB_PATH'/CellRanger/refdata-gex-mm10-2020-A'
+
+#################################################
+## 3. Run CellRanger
+#################################################
+
+cd $WKD'/cellranger_out'
+
+while IFS=";" read -r sample_id flowcells fastqs; 
+do
+
+# --nosecondary was deleted from exec to generate cluster output
+  singularity exec $IMAGES_PATH/$CELLRANGER cellranger count \
+                --include-introns true \
+                --id=$sample_id \
+                --sample=$flowcells \
+                --fastqs=$RAWDATA/$fastqs \
+                --transcriptome=$REF
+
+done < $SAMPLESHEET
+
+#######################
+## 4. End Preprocessing
+#######################
+
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+echo "Processing Time: $DIFF seconds"
