c3ecffa9fe13480b4e47a73966de2c5711929aa8
María Maqueda González <mmaqueda@live.com>
2024-10-07 12:38:35 +0200

    Update 5_Mouse_Cell_Atlas.R

--- scRNAseq_10x_BoneMarrow/5_Mouse_Cell_Atlas.R
+++ scRNAseq_10x_BoneMarrow/5_Mouse_Cell_Atlas.R
@@ -8,9 +8,8 @@
 
 ##### Import libraries #####
 
-libraries <- c("knitr", "ggplot2", "data.table", "SingleCellExperiment", "dplyr", 
-               "stringr", "ggpubr", "Seurat", "glmGamPoi", "clustree", "multtest",
-               "metap","ComplexHeatmap", "RColorBrewer", "SingleR", "scmap") 
+libraries <- c("SingleCellExperiment", "dplyr", 
+               "stringr", "ggpubr", "Seurat") 
 check.libraries <- is.element(libraries, installed.packages()[, 1])==FALSE
 libraries.to.install <- libraries[check.libraries]
 if (length(libraries.to.install!=0)) {
@@ -22,6 +21,10 @@ if(length(success) != length(libraries)) {stop("A package failed to return a suc
 
 
 ##### Import data #####
+
+# REMARK: Data was directly downloaded (July'24) from https://bis.zju.edu.cn/MCA/gallery.html?tissue=Bone-Marrow
+# and imported into R environment. From MCA website > Gallery > Bone-Marrow > Bone-Marrow (adult, with 9049 cells)
+
 
 # Data frame with expression data (First row contains row numbers, so we do not need to import it)
 bm_atlas <- read.csv("/home/user/Documents/Files/Projects/BM_scRNAseq/Bone-Marrow/Bone-Marrow_dge.csv")[-1]
